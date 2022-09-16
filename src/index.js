994dc2f503ebb689e27fb66b216af0b148153aad
resqnet <resqnet52@gmail.com>
2022-09-16 11:53:14 +0900

    fix filepath

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import * as path from 'path';
 import {parse, toJson} from './parser.js';
 
 const resultDirPath = ".lighthouseci";
+//const resultDirPath = "score";
+
 
 const resultJsons = fs.readdirSync(`./${resultDirPath}`, {withFileTypes: true})
     .filter(dirent => dirent.isFile())
@@ -10,6 +12,7 @@ const resultJsons = fs.readdirSync(`./${resultDirPath}`, {withFileTypes: true})
     .filter(file => path.extname(file).toLowerCase() === ".json");
 
 resultJsons.forEach(file => {
+    console.log("Initialize:" + file);
     const data = parse(toJson(`./${resultDirPath}/${file}`));
-    console.log(data);
+    //console.log(data);
 });
